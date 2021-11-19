fd60a35c7466f2c6139a0581f3b3cd10fd2ba3e7
Teo-Fer <62347349+Teo-Fer@users.noreply.github.com>
2021-11-19 11:39:35 -0300

    Add files via upload

--- TuringMachine.js
+++ TuringMachine.js
@@ -1,14 +1,41 @@
 const fs = require("fs");
 
-function readCsv(path) {
-  let csv;
-  fs.readFile("./input.csv", "utf8", (err, data) => {
+function readFile(path) {
+  let file = fs.readFileSync(path, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    csv = data;
-    console.log(data);
+    return data;
   });
-  console.log(csv);
+  return file;
 }
+
+function csvToMatrix(fileCsv) {
+    let lines = fileCsv.split(/\r\n?\n/);
+    let alphabet = lines[0].split(",");
+    console.log("alfabeto: ",alphabet);
+    let matrix = new Array();
+    for (let row = 1; row < lines.length; row++){
+      let rowSeparated = [];
+      rowSeparated = lines[row].split(",");
+      for (let colum = 0; colum < rowSeparated.length; colum++){
+          matrix[row-1] = rowSeparated;
+      }
+    }
+    return matrix;
+}
+
+function readMatrix(matrix, input) {
+  colum = input.split("");
+  console.log(colum);
+  element = matrix[0][0].split("/");
+  nextState = 
+  console.log(element);
+}
+let input = "11";
+let fileCsv = readFile("./input.csv");
+let matrix = csvToMatrix(fileCsv);
+readMatrix(matrix, input);
+
+
