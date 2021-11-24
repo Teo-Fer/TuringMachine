851a17b437333f39cc677053881b4f1330370572
Teo-Fer <62347349+Teo-Fer@users.noreply.github.com>
2021-11-24 15:11:29 -0300

    Add files via upload

--- TuringMachine.js
+++ TuringMachine.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+var LAMBDA = "$"
+var alphabet;
+var head = 0;
+
+let input = "dcbaa";
+
+function setCharAt(str,index,chr) {
+  if(index > str.length-1) return str;
+  return str.substring(0,index) + chr + str.substring(index+1);
+}
+
 function readFile(path) {
   let file = fs.readFileSync(path, "utf8", (err, data) => {
     if (err) {
@@ -13,7 +24,7 @@ function readFile(path) {
 
 function csvToMatrix(fileCsv) {
     let lines = fileCsv.split(/\r\n?\n/);
-    let alphabet = lines[0].split(",");
+    alphabet = lines[0].split(",");
     console.log("alfabeto: ",alphabet);
     let matrix = new Array();
     for (let row = 1; row < lines.length; row++){
@@ -23,17 +34,74 @@ function csvToMatrix(fileCsv) {
           matrix[row-1] = rowSeparated;
       }
     }
+    console.log("Matriz:\n",matrix);
     return matrix;
 }
 
-function readMatrix(matrix, input) {
-  colum = input.split("");
-  console.log(colum);
-  element = matrix[0][0].split("/");
-  nextState = 
-  console.log(element);
+function printTape(charToWrite){
+  if (charToWrite != LAMBDA){
+      input = setCharAt(input, head, charToWrite);
+  }
+  let prettyOutPut="";
+  input.split("").forEach(e =>{
+    prettyOutPut+=e+" ";
+  })
+  console.log(prettyOutPut);
 }
-let input = "11";
+
+function printHead(movement, tape){
+  let marker = "";
+  for (let i = 0; i< head;i++){
+    marker+="  "
+  }
+  marker+="^"
+  console.log(marker);
+  switch (movement) {
+    case "R":
+      if (head == tape.length) {
+        head = 0;
+      } else{
+        head += 1;
+      }
+      break;
+    case "L":
+      if (head == 0) {
+        head = tape.length-1;
+      } else {
+        head -= 1; 
+      }
+      break;
+    case "N":
+      break;
+  }
+ 
+}
+
+function readMatrix(matrix) {
+  let colum = 0;
+  let row = 0;
+  let prettyOutPut="";
+  input.split("").forEach(e =>{
+    prettyOutPut+=e+" ";
+  })
+  console.log("Entrada: " + prettyOutPut);
+
+  element = matrix[row][colum].split("/");
+  input.split("").forEach(char => {
+    row = element[0]-1;
+    colum = alphabet.indexOf(char);
+    element = matrix[row][colum].split("/");
+    console.log("row: "+ row+"  colum: "+ colum);
+    movement = element[1];
+    console.log("Realiza el movimiento: "+ movement);
+    charToWrite = element[2];
+    console.log("Escribe el valor: "+ charToWrite);
+    printTape(charToWrite);
+    printHead(movement, input);
+  });
+  
+}
+let entrada = window.prompt('Ingrese su nombre:','');
 let fileCsv = readFile("./input.csv");
 let matrix = csvToMatrix(fileCsv);
 readMatrix(matrix, input);
